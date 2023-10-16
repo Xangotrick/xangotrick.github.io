45f75b245f7aef4f8fbdc78b87bf217f9fd90dc7
Xangotrick <michael.arthur.montana@gmail.com>
2023-10-16 08:43:12 +0200

    Add files via upload

---
+++ valheim/valheim.js
@@ -0,0 +1,53 @@
+var button = document.getElementById("mybutton")
+function addNumbers () {
+    var img = document.getElementById("myimg")
+    var txt = document.getElementById("myinput").value
+    switch(txt)
+    {
+        case("6VTEJ"):
+            img.src = "6VTEJ.png";
+            break;
+        case("PC3K5"):
+            img.src = "PC3K5.png";
+            break;
+        case("QYYJ5"):
+            img.src = "QYYJ5.png";
+            break;
+        case("PPISF"):
+            img.src = "PPISF.png";
+            break;
+        case("4XHYS"):
+            img.src = "4XHYS.png";
+            break;
+        case("GGWEB"):
+            img.src = "GGWEB.png";
+            break;
+        case("G2OOT"):
+            img.src = "G2OOT.png";
+            break;
+        case("AHR3C"):
+            img.src = "AHR3C.png";
+            break;
+        case("WIYCB"):
+            img.src = "WIYCB.png";
+            break;
+        case("8XZQF"):
+            img.src = "8XZQF.png";
+            break;
+        case("8BL5G"):
+            img.src = "8BL5G.png";
+            break;
+        case("9IW4F"):
+            img.src = "9IW4F.png";
+            break;
+        case("SMV5U"):
+            img.src = "SMV5U.png";
+            break;
+        case("X8RSP"):
+            img.src = "X8RSP.png";
+            break;
+        case("ZKYIL"):
+            img.src = "ZKYIL.png";
+            break;
+    }
+}
